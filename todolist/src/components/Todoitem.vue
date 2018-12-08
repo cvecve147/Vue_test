c33634ed40e068bd669a3b979f58575ba6e538bd
cvecve147 <cplyuef357@gmail.com>
2018-12-08 20:04:53 +0800

    vue cli

---
+++ todolist/src/components/Todoitem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li
+    class="item"
+    @click="handleDelete"
+  >{{content}}</li>
+</template>
+
+<script>
+  export default {
+    props: ["content", "index"],
+    methods: {
+      handleDelete() {
+        this.$emit("del", this.index);
+      }
+    }
+  };
+</script>
+ 
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  .item {
+    color: blue;
+  }
+</style>
---
+++ todolist/src/todolist.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <div>
+      <input
+        type="text"
+        v-model="inputValue"
+        class="item"
+      >
+      <button @click="handleSubmit">提交</button>
+    </div>
+    <ul>
+      <todo-item
+        v-for="(item,index) of list"
+        :content="item"
+        :key="index"
+        :index="index"
+        @del="handleDel"
+      ></todo-item>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+  import Todoitem from "./components/Todoitem";
+  export default {
+    components: { "todo-item": Todoitem },
+    data() {
+      return {
+        inputValue: "",
+        list: []
+      };
+    },
+    methods: {
+      handleSubmit() {
+        this.list.push(this.inputValue);
+        this.inputValue = "";
+      },
+      handleDel(index) {
+        this.list.splice(index, 1);
+      }
+    }
+  };
+</script>
+
+<style>
+</style>
